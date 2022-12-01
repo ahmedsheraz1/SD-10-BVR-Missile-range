158d8f525785f353cacc384879267a19ae8d056f
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-01 10:06:41 +0500

    Create SD-10 BVR Missile range.py

---
+++ SD-10 BVR Missile range.py
@@ -0,0 +1,15 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+r = range(0, 70, 2)
+r
+range(0, 70, 2)
+10 in r
+True
+r.index(10)
+5
+r[5]
+10
+r[:5]
+range(0, 10, 2)
+r[-1]
+68
